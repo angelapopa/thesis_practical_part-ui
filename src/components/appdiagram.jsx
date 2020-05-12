c1b285e58a7e70921847ccd188ca2c46a922be4c
Angela Popa <angela.popa@student.uibk.ac.at>
2020-05-12 21:41:36 +0200

    redrawing app diagrams

--- src/components/appdiagram.jsx
+++ src/components/appdiagram.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
-import epcAppDiagram from "../resources/app-diagram.png";
+import epcDataImport from "../resources/app-diagram/data-import.png";
+import epcAppArchitecture from "../resources/app-diagram/app-architecture.png";
 
 class AppDiagram extends Component {
   render() {
+    const styleObj = {
+      fontSize: 10,
+    };
+
     console.log("AppDiagram - Rendered");
     return (
       <React.Fragment>
-        <img src={epcAppDiagram} alt="app-diagram" width={1200}></img>
+        <div>
+          <h1>&nbsp;</h1>
+          <h3>Data Import</h3>
+        </div>
+        <img src={epcDataImport} alt="data-import" width={700}></img>
+        <div>
+          <h1>&nbsp;</h1>
+          <h3>Application Architecture</h3>
+          <h3>&nbsp;</h3>
+        </div>
+        <img src={epcAppArchitecture} alt="app-diagram" width={1200}></img>
+        <div>
+          <span style={styleObj}>Image Sources: </span>
+          <span style={styleObj}>
+            MongoDB Logo (http://getdrwaings.com/mongodb-icon),
+          </span>
+          <span style={styleObj}>
+            NodeJS Logo (https://pngimage.net/nodejs-logo-png-8)
+          </span>
+        </div>
       </React.Fragment>
     );
   }
