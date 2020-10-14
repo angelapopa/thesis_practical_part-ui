582e3e2c7e19f7c79bb983ad3af1e6ffe6726619
Angela Popa <angela.popa@student.uibk.ac.at>
2020-10-14 20:12:16 +0200

    adding new app diagram

--- src/components/appdiagram.jsx
+++ src/components/appdiagram.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import epcDataImport from "../resources/app-diagram/data-import.png";
-import epcAppArchitecture from "../resources/app-diagram/app-architecture.png";
+import epcDataImportV100 from "../resources/app-diagram/v1-0-0/data-import.png";
+import epcAppArchitectureV100 from "../resources/app-diagram/v1-0-0/app-architecture.png";
+import epcDataImportV200 from "../resources/app-diagram/v2-0-0/data-import-v2.0.0.png";
+import epcAppArchitectureV200 from "../resources/app-diagram/v2-0-0/app-architecture-v2.0.0.png";
 
 class AppDiagram extends Component {
   render() {
@@ -18,15 +20,49 @@ class AppDiagram extends Component {
         <div style={marginObj}>
           <div>
             <h1>&nbsp;</h1>
+            <h1>v2.0.0</h1>
+          </div>
+          <div>
+            <h1>&nbsp;</h1>
             <h3>Data Import</h3>
           </div>
-          <img src={epcDataImport} alt="data-import" width={650}></img>
+          <img src={epcDataImportV200} alt="data-import" width={1050}></img>
+          <div>
+            <h1>&nbsp;</h1>
+            <h1>&nbsp;</h1>
+            <h3>Application Architecture</h3>
+          </div>
+          <img src={epcAppArchitectureV200} alt="app-diagram" width={1200}></img>
+          <div>
+            <h3>&nbsp;</h3>
+            <span style={styleObj}>Image Sources: </span>
+            <span style={styleObj}>
+              MongoDB Logo (http://getdrwaings.com/mongodb-icon),
+            </span>
+            <span style={styleObj}>
+              NodeJS Logo (https://pngimage.net/nodejs-logo-png-8)
+            </span>
+          </div>
+        </div>
+        <div style={marginObj}>
+          <div>
+            <h1>&nbsp;</h1>
+            <h1>&nbsp;</h1>
+            <h1>&nbsp;</h1>
+            <h1>&nbsp;</h1>
+            <h1>v1.0.0</h1>
+          </div>
+          <div>
+            <h1>&nbsp;</h1>
+            <h3>Data Import</h3>
+          </div>
+          <img src={epcDataImportV100} alt="data-import" width={650}></img>
           <div>
             <h1>&nbsp;</h1>
             <h3>Application Architecture</h3>
             <h3>&nbsp;</h3>
           </div>
-          <img src={epcAppArchitecture} alt="app-diagram" width={1200}></img>
+          <img src={epcAppArchitectureV100} alt="app-diagram" width={1200}></img>
           <div>
             <span style={styleObj}>Image Sources: </span>
             <span style={styleObj}>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -23,7 +23,7 @@ const NavBar = ({ totalCounters }) => {
           <Nav.Link href="/uml">UML representation</Nav.Link>
         </Nav>
       </Navbar.Collapse>
-      <Navbar.Text>v1.3.0</Navbar.Text>
+      <Navbar.Text>v2.0.0</Navbar.Text>
     </Navbar>
   );
 };
